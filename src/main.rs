11529b666102dfb1e0d117c232eaca66ee1ab6fe
Tejas <tejas75o25@gmail.com>
2022-10-20 16:18:43 +0530

    removed the warnings :)

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 mod loader;
 
 fn main() {
-    stdout().write_all(b"\x1B[2J\x1B[1;0H\x1B[?25l");
+    stdout().write_all(b"\x1B[2J\x1B[1;0H\x1B[?25l").expect("error occurred!");
 
     let mut csv_parser_frontend = loader::Manager::new("1. parsing data.csv");
     thread::sleep(Duration::from_secs(2));
@@ -17,5 +17,5 @@ fn main() {
     thread::sleep(Duration::from_secs(6));
     ml_training_frontend.end("success");
 
-    stdout().write_all(b"\x1B[?25h");
+    stdout().write_all(b"\x1B[?25h").expect("error occurred!");
 }
